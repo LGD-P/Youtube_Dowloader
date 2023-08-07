00bb7834dbc701e58be0e656cfab6b1746ecb1ab
LGD-P <nplgdjp@gmx.Com>
2023-08-07 12:46:24 +0200

    add youtube playlist to video

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class YoutubeDlModel:
     @staticmethod
     def download_audio(link, output_path):
         """
-        Choose the right method based on a playlist or simple URL 
+        Choose the right audio method based on a youtube playlist or simple URL
         """
         if YoutubeDlModel.PLAYLIST_TAG in link:
             print("C'est une playlist:")
@@ -53,6 +53,25 @@ class YoutubeDlModel:
             for link in filtered_list:
                 YoutubeDlModel.download_audio(link, output_path)
 
+    @staticmethod
+    def download_video(link, output_path):
+        """
+        Choose the right audio method based on a youtube playlist or simple URL
+        """
+        if YoutubeDlModel.PLAYLIST_TAG in link:
+            print("C'est une playlist:")
+            print(link)
+            for video in Playlist(link).videos:
+                print(video.embed_url)
+                video.streams.filter(progressive=True, file_extension='mp4').order_by(
+                    'resolution').desc().first().download(output_path=output_path)
+
+        else:
+            print("C'est un simple lien")
+            print(link)
+            YouTube(link).streams.filter(progressive=True).order_by(
+                'resolution').desc().first().download(output_path=output_path)
+
     @ staticmethod
     def select_path_and_download_video(link):
         """This function allows the user to select a path
@@ -62,7 +81,7 @@ class YoutubeDlModel:
             .mp4: Audio from youtube link as .mp4
         """
         path = filedialog.askdirectory()
-        return YouTube(link).streams.filter(progressive=True).get_highest_resolution().download(path)
+        YoutubeDlModel.download_video(link, path)
 
     @ staticmethod
     def download_from_list_video():
@@ -77,7 +96,6 @@ class YoutubeDlModel:
                 first_list.append(element.strip())
                 filtered_list = list(filter(None, first_list))
             path_to_download = filedialog.askdirectory()
-            for url in filtered_list:
-                print(url)
-                YouTube(url).streams.filter(progressive=True).get_highest_resolution().download(
-                    path_to_download)
+            for link in filtered_list:
+                print(link)
+                YoutubeDlModel.download_video(link, path_to_download)
